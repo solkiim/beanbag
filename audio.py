4132965d5d7896a041d33bef9f9130a10f414e7e
Sol Kim <solkim813@gmail.com>
2018-12-03 10:09:41 -0500

    nit

--- audio.py
+++ audio.py
@@ -1,6 +1,6 @@
 import pygame
 pygame.mixer.init()
-pygame.mixer.music.load("test.wav")
+pygame.mixer.music.load("r2d2.wav")
 pygame.mixer.music.play()
 while pygame.mixer.music.get_busy() == True:
 	continue
